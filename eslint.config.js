b6a921d81ea93abb5ef1c2b34e23cbc4b1309243
muryllovieira <muryllovieira@gmail.com>
2026-04-14 18:13:54 -0300

    feat: implementation CI/CD

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    // Adicione isso:
+    ignores: ['node_modules/', '.expo/', 'dist/', 'components/'],
+  },
+];
